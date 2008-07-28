db19e1a23b183a297f7556db09e8c2ff3d70e1c8
James A. Duncan <jamesaduncan@mac.com>
2008-07-28 09:16:26 -0400

    HTTP Request Extension

---
+++ lib/RSP/Extension/HTTP.pm
@@ -0,0 +1,44 @@
+package RSP::Extension::HTTP;
+
+use strict;
+use warnings;
+
+use LWPx::ParanoidAgent;
+use HTTP::Request;
+
+our $VERSION = '1.00';
+
+sub provide {
+  my $class = shift;
+  my $tx    = shift;
+  my $ua = LWPx::ParanoidAgent->new;
+  $ua->agent("Reasonably Smart Platform / HTTP / $VERSION");
+  $ua->timeout( 10 );  
+  return (
+    'http' => {
+      'request' => sub {
+        my $response = eval {
+          my $req = shift;
+          my $r = HTTP::Request->new( @$req );
+          $ua->request( $r );
+        };
+        if ($@) {
+          die "error: $@";
+        }
+        return $class->response_to_object( $response );
+      }
+    }
+  );
+}
+
+sub response_to_object {
+  my $class = shift;
+  my $response = shift;
+  my %headers = %{ $response->{_headers} };
+  return {
+    'headers'=> \%headers,
+    'content' => $response->decoded_content
+  }
+}
+
+1;
