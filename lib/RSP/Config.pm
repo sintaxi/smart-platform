b4d6859d509526cc3c15708596dd2eefed61547b
James A. Duncan <jamesaduncan@mac.com>
2008-07-29 09:49:46 -0400

    much more efficient access of the config file, not parsing every time we read, only update if it changes

--- lib/RSP/Config.pm
+++ lib/RSP/Config.pm
@@ -8,14 +8,31 @@ use File::Spec;
 
 our $AUTOLOAD;
 our @CONFIGDIR = ( './etc','/etc' );
+our $MTIME = 0;
+our $CONFIG;
+our $CONFIG_FILENAME;
 
 sub RSP::config {
   my $class = shift;  
+  $CONFIG_FILENAME ||= $class->find_config_file;
+  my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+    $atime,$mtime,$ctime,$blksize,$blocks)
+        = stat($CONFIG_FILENAME);
+  if ( $mtime > $MTIME ) {
+    print STDERR "reading config file\n";
+    $CONFIG = Config::Tiny->read( $CONFIG_FILENAME );
+    $MTIME  = $mtime;
+    return $CONFIG;
+  } else {
+    return $CONFIG;
+  }
+}
+
+sub find_config_file {
   foreach my $dir ( @CONFIGDIR ) {
-    my $cf = File::Spec->catfile( $dir, 'rsp.conf' );
+    my $cf = File::Spec->catfile( $dir, 'rsp.conf' );  
     if (-e $cf && -f $cf ) {
-      my $config = Config::Tiny->read( $cf );
-      return $config;
+      return $cf;
     }
   }
 }
