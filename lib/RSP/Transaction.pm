80106f3ea40b3840181e4a9f7af03884fe82192c
James A. Duncan <jamesaduncan@mac.com>
2009-01-03 09:36:00 -0500

    sub-class of transaction for mojo specifically

--- lib/RSP/Transaction.pm
+++ lib/RSP/Transaction.pm
@@ -14,7 +14,11 @@ our $HOST_CLASS = RSP->config->{_}->{host_class} || 'RSP::Host';
 __PACKAGE__->mk_accessors(qw( request response runtime context hostclass ));
 
 sub import {
+  my $class = shift;
   Module::Load::load( $HOST_CLASS );
+  eval {
+    $class->SUPER::import(@_);
+  };
 }
 
 ##
@@ -122,51 +126,7 @@ sub run {
   }
 }
 
-sub encode_response {
-  my $self = shift;
-  my $response = shift;
 
-  my @resp = @$response;
-
-  my ($code, $codestr, $headers, $body) = @resp;
-  $self->response->code( $code );
-  my @headers = @$headers;
-  while( my $key = shift @headers ) {
-    my $value = shift @headers;
-    ## why do we need to special case this?
-    if ( $key eq 'Set-Cookie') {
-      my $cookies = Mojo::Cookie::Response->new->parse( $value );
-      $self->response->cookies( $cookies->[0] );
-    } else {
-      $self->response->headers->add_line( $key, $value );
-    }
-  }
-  
-  ##
-  ## if we have a simple body string, use that, otherwise
-  ##  we need to be a bit more clever
-  ##
-  if (!ref($body)) {
-    $self->response->body( $body );
-  } else {
-    if ( ref($body) eq 'JavaScript::Function' ) {
-      ## it's a javascript function, call it and use the
-      ## returned data
-      $self->response->body( $body->() );
-    } elsif ( ref($body) && $body->isa('RSP::JSObject') ) {
-      ##
-      ## it's a blended object, most likely ( at this point ) a file.
-      ##   suck the data up and use that
-      ##
-      $self->response->body( $body->as_string );
-    } else {
-      ##
-      ## we don't know what to do with it.
-      ##
-      die "don't know what to do with " . ref($body) . " object";
-    }
-  }
-}
 
 ##
 ## terminates the transaction
@@ -207,6 +167,7 @@ sub import_extensions {
 sub host {
   my $self = shift;  
   
+  ## allow for more "pluggable" host classes...
   my $host_class = $self->hostclass || $HOST_CLASS;
   if ( !$self->{host} ) {
     $self->{host} = $host_class->new( $self ); 
