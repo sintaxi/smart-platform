58815f91b123db293e733099cd1244ffb8970738
James A. Duncan <james@joyent.com>
2009-02-03 09:47:15 -0500

    handle JSObjects

--- lib/RSP/Extension/MD5.pm
+++ lib/RSP/Extension/MD5.pm
@@ -2,6 +2,8 @@ package RSP::Extension::MD5;
 
 use strict;
 use warnings;
+
+use Scalar::Util qw( blessed );
 use Digest::MD5 qw( md5_hex md5_base64 );
 
 use base 'RSP::Extension';
@@ -15,13 +17,18 @@ sub provides {
       md5 => {
         'hex' => sub {
           my $data = shift;
+	  if ( blessed( $data ) ) {
+	    return md5_hex( $data->as_string );
+	  }
           return md5_hex( $data );
         },
         'base64' => sub {
           my $data = shift;
+	  if ( blessed( $data ) ) {
+	    return md5_hex( $data->as_string );
+	  }
           return md5_base64( $data );
         },
-        
       }
     }
   }
