c207dcd57e3dd401ef6e58ac8558e672019b5183
James A. Duncan <james@joyent.com>
2009-02-03 22:55:09 -0500

    we need to do things differently because we need to handle multipart form data

--- lib/RSP/Extension/HTTPRequest.pm
+++ lib/RSP/Extension/HTTPRequest.pm
@@ -3,6 +3,9 @@ package RSP::Extension::HTTPRequest;
 use strict;
 use warnings;
 
+use File::Basename qw( basename );
+use RSP::JSObject::File;
+
 use base 'RSP::Extension';
 
 sub should_provide {
@@ -23,21 +26,40 @@ sub provides {
       $cookies->{$name} = "$value";
     }
   }
-  
-  return {
-    'request' => {
-      'uri'    => $tx->request->url->path->to_string,
-      'method' => $tx->request->method,
-      'query'  => $tx->request->query_params->to_hash,
-      'body'   => $tx->request->body_params->to_hash,
-      'headers'=> { map {
-        ( lc($_) => $tx->request->headers->header($_) )
-      } @{$tx->request->headers->names} },
-      'queryString' => $tx->request->url->query->to_string,
-      'cookies' => $cookies,
-      'content' => $tx->request->body
-    }  
+
+  my $request = {};
+  eval {
+    $request->{uri}     = $tx->request->url->path->to_string;
+    $request->{method}  = $tx->request->method;
+    $request->{query}   = $tx->request->query_params->to_hash;
+    $request->{body}    = $tx->request->body_params->to_hash;
+    $request->{cookies} = $cookies;
+
+
+    if ( $tx->request->is_multipart ) {
+      $request->{uploads} = {
+			     map {
+			       my $name = $_->name;
+			       my $file = $_->file;
+			       ( $name => RSP::JSObject::File->new( $file->path, basename( $_->filename ) ) )
+			     } @{ $tx->request->uploads }
+			    };
+    } else {
+      $request->{content} = $tx->request->body;
+    }
+    $request->{headers} = {
+			   map {
+			     ( lc($_) => $tx->request->headers->header($_) )
+			   } @{ $tx->request->headers->names }
+			  };
+
+    $request->{queryString} = $tx->request->url->query->to_string;
   };
+
+  if ($@) {
+    print "couldn't bind request: $@\n";
+  }
+  return { request => $request };
 }
 
 1;
