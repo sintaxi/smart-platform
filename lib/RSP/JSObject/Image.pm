7a04a1095908da98c82b3b5e1e3ba5fceae40652
James A. Duncan <james@joyent.com>
2009-02-12 13:23:48 -0500

    store the mimetype of the image when we create the image object in js space...

--- lib/RSP/JSObject/Image.pm
+++ lib/RSP/JSObject/Image.pm
@@ -6,8 +6,10 @@ use Imager;
 use Image::Math::Constrain;
 use MIME::Types;
 
+use File::Temp;
+
 use base 'RSP::JSObject';
-__PACKAGE__->mk_accessors(qw( imager file ));
+__PACKAGE__->mk_accessors(qw( imager file mimetype ));
 
 sub jsclass {
   return 'RSPImage';
@@ -23,14 +25,13 @@ sub constructor {
 
 sub new {
   my $class = shift;
-  use Data::Dumper; print Dumper( \@_ );
   my $file  = shift;
   if (!$file) {
     die { message => 'no file object' };
   }
   my $img   = Imager->new;
   $img->read( file => $file->fullpath ) or die $img->errstr;
-  my $self  = { file => $file, imager => $img };
+  my $self  = { file => $file, imager => $img, mimetype => MIME::Type->new( type => $file->mimetype ) };
   bless $self, $class;
 }
 
@@ -70,14 +71,29 @@ sub methods {
 	  'scale'  => sub {
 	    my $self = shift;
 	    my $opts = shift;
-	    my $x    = $opts->{xpixels} // 0;
-	    my $y    = $opts->{ypixels} // 0;
+	    my $x    = $opts->{xpixels} // 0; ## 5.10 feature, defined xpixels or 0.
+	    my $y    = $opts->{ypixels} // 0; ## 5.10 feature, defined xpixels or 0.
 	    $self->imager( $self->imager->scale( constrain => Image::Math::Constrain->new( $x, $y ) ) );
 	  },
 	  'crop'  => sub {
 	    my $self = shift;
 	    my $opts = shift;
 	    $self->imager( $self->imager->crop( %$opts ) || die $self->imager->errstr );
+	  },
+	  'save'  => sub {
+	    my $self = shift;
+	    my $new  = shift;
+	    if ( $new ) {
+	      my $temp = File::Temp->new;
+	      $self->imager->write( file => $temp->filename, type => $self->mimetype->subType )
+		or die { message => $self->imager->errstr };
+	      $temp->close;
+	      $self->file( RSP::JSObject::File->new( $temp->filename, "tmpimage" ) );
+	    }
+	    $self->imager->write( file => $self->file->fullpath, type => $self->mimetype->subType )
+	      or die { message => $self->imager->errstr };
+
+	    return $self->file;
 	  }
 	 }
 }
