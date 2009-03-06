9b47534e2e872eebf94118bcfcf2b413da60ba55
James A. Duncan <james@joyent.com>
2009-03-06 14:07:32 -0500

    more build info

--- lib/RSP.pm
+++ lib/RSP.pm
@@ -5,30 +5,15 @@ use warnings;
 
 use Cwd;
 use base 'Mojo';
-use Number::Bytes::Human 'format_bytes';
-use Darwin::Process::Memory;
 use Scalar::Util qw( weaken );
 our $VERSION = '1.2';
 use Application::Config 'rsp.conf';
 
 use RSP::Transaction::Mojo;
 
-sub sample_memory {
-  return (
-      #format_bytes( 
-	  Darwin::Process::Memory::resident(),
-      #),
-      #format_bytes(
-	  Darwin::Process::Memory::virtual()
-      #),
-      );
-}
-
 sub handler {
   my ($self, $tx) = @_;
 
-  my ($rstart, $vstart) = sample_memory();
-  print STDERR sprintf("$$ start: resident: %s, virtual: %s\n", $rstart, $vstart);
   my $rsptx = RSP::Transaction::Mojo->new
                                     ->request( $tx->req )
 				    ->response( $tx->res );
@@ -45,9 +30,6 @@ sub handler {
   $rsptx->request( undef );
   $rsptx->response( undef );
   $rsptx = undef;
-
-  my ($rend, $vend) = sample_memory();
-  print STDERR sprintf("$$ end: resident: %s, virtual: %s, resident diff: %s\n", $rend, $vend, format_bytes( $rend - $rstart ));
 
   return $tx;
 }
