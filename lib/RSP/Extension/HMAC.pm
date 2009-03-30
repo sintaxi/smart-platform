9660b24e507ed45d0f4c61f271b1524f85c49692
James A. Duncan <james@joyent.com>
2009-03-30 11:47:20 -0400

    whoops, really we want the base64 and hex HMAC digests

--- lib/RSP/Extension/HMAC.pm
+++ lib/RSP/Extension/HMAC.pm
@@ -16,20 +16,25 @@ sub provides {
   return {
 	  'digest' => {
 		       'hmac' => {
-				  'sha1' => sub {
-				    my ($data, $key) = @_;
-				    if ( blessed( $data ) ) {
-				      return hmac_sha1( $data->as_string, $key );
-				    }
-				    return hmac_sha1( $data, $key );
-				  },
-				  'sha1_hex' => sub {
-				    my ($data, $key) = @_;
-				    if ( blessed( $data ) ) {
-				      return hmac_sha1_hex( $data->as_string, $key );
-				    }
-				    return hmac_sha1_hex( $data, $key );
-				  }
+			   'sha1' => {
+				      'base64' => sub {
+					my ($data, $key) = @_;
+					my $dig = Digest::HMAC_SHA1->new( $key );
+					if ( blessed( $data ) ) {
+					  $dig->add( $data->as_string );
+					} else {
+					  $dig->add( $data );
+					}
+					return $dig->b64digest;
+				      },
+				      'hex' => sub {
+					my ($data, $key) = @_;
+					if ( blessed( $data ) ) {
+					  return hmac_sha1_hex( $data->as_string, $key );
+					}
+					return hmac_sha1_hex( $data, $key );
+				      }
+				     }
 				 }
 		      }
 	 }
