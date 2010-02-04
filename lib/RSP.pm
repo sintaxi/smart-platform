838c388b7f7218f5f8e420b80a693f32472c44fb
Scott McWhirter <scott.mcwhirter@joyent.com>
2010-02-04 13:47:02 -0800

    Update RSP to allow for platform-wide changes to be allowed for extensions

--- lib/RSP.pm
+++ lib/RSP.pm
@@ -1,8 +1,6 @@
 package RSP;
 
-use strict;
-use warnings;
-
+use Moose;
 use Cwd;
 use base 'Mojo';
 use Scalar::Util qw( weaken );
@@ -18,6 +16,31 @@ sub conf {
         $CONFIG = RSP::Config->new(config => { %{ $class->config } });
     }
     return $CONFIG;
+}
+
+sub BUILD {
+    my ($self) = @_;
+
+    my @extension_stack = @{ $self->conf->available_extensions };
+    my $unable_to_comply = {};
+
+    while(my $ext = shift(@extension_stack)){
+        my $class = $ext;
+        Class::MOP::load_class($class);
+
+        if($class->can('does') && $class->does('RSP::Role::AppMutation')){
+            warn "Attempting to apply mutations on $class\n";
+            if($class->can_apply_mutations($self->config)){
+                $class->apply_mutations($self->config);
+            } else {
+                push(@extension_stack, $class);
+                my $tries = ++$unable_to_comply->{$class};
+                if($unable_to_comply > scalar(@extension_stack)){
+                    die "Unable to apply extension mutations";
+                }
+            }
+        }
+    }
 }
 
 use RSP::Transaction::Mojo;
